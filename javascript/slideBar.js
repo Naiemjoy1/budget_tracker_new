7910c9e445e9db2b16f2bc2379fbc7fa83bd7b0f
naiemjoy1 <naiemunhasan@gmail.com>
2025-01-06 14:29:45 +0600

    hamburg menu

---
+++ javascript/slideBar.js
@@ -0,0 +1,17 @@
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const sidebar = document.getElementById("sidebar");
+const closeSidebar = document.querySelector(".close-sidebar");
+
+hamburgerIcon.addEventListener("click", () => {
+    sidebar.classList.add("show");
+});
+
+closeSidebar.addEventListener("click", () => {
+    sidebar.classList.remove("show");
+});
+
+window.addEventListener("click", (event) => {
+    if (!sidebar.contains(event.target) && !hamburgerIcon.contains(event.target)) {
+        sidebar.classList.remove("show");
+    }
+});
